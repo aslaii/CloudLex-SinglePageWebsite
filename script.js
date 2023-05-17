c3a0848eecee4dc15d9b4e11a0139144c0af3cf9
Jericho Bermas <jecho.deleon@gmail.com>
2023-05-17 13:27:42 +0800

    Fixed some js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-$(document).ready(function () {
-
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
-
-        // scroll spy
-        $('section').each(function () {
-            let height = $(this).height();
-            let offset = $(this).offset().top - 200;
-            let top = $(window).scrollTop();
-            let id = $(this).attr('id');
-
-            if (top > offset && top < offset + height) {
-                $('.navbar ul li a').removeClass('active');
-                $('.navbar').find(`[href="#${id}"]`).addClass('active');
-            }
-        });
-    });
-
 
 //typed.js
 var typed = new Typed(".typing-text", {
@@ -57,16 +26,6 @@ function animateWithRandomNumber(myClass, from, to) {
   //tilt js
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 15,
-});
-
-$('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top,
-    }, 500, 'linear')
-});
-
-
 });
 
 const srtop = ScrollReveal({
